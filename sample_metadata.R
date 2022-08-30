b44ee63843ea59a8ef4db5b3203dfd13e75bcd17
Ben John Ober-Reynolds <boberrey@sh03-ln05.stanford.edu>
2022-08-30 14:50:52 -0700

    added sample metadata

---
+++ sample_metadata.R
@@ -0,0 +1,56 @@
+# Sample metadata
+
+samp.preservation <- list(
+    "C_SD1"="fresh",
+    "C_SD2"="fresh",
+    "C_SD3"="fresh",
+    "C_SD4"="frozen",
+    "C_SD5"="frozen",
+    "C_SD6"="frozen",
+    "C_SD7"="frozen",
+    "AA1"="fresh",
+    "AA2"="fresh",
+    "AA4"="fresh",
+    "AA7"="frozen",
+    "AA8"="frozen",
+    "C_PB1"="fresh",
+    "C_PB2"="fresh",
+    "C_PB3"="fresh"
+)
+
+samp.sex <- list(
+    "C_SD1"="F",
+    "C_SD2"="F",
+    "C_SD3"="F",
+    "C_SD4"="M",
+    "C_SD5"="M",
+    "C_SD6"="F",
+    "C_SD7"="M",
+    "AA1"="F",
+    "AA2"="M",
+    "AA4"="F",
+    "AA7"="F",
+    "AA8"="F",
+    "C_PB1"="M",
+    "C_PB2"="F",
+    "C_PB3"="F"
+)
+
+# Rounded to nearest 5 years
+samp.age <- list(
+    "C_SD1"=45,
+    "C_SD2"=45,
+    "C_SD3"=65,
+    "C_SD4"=75,
+    "C_SD5"=80,
+    "C_SD6"=65,
+    "C_SD7"=65,
+    "AA1"=40,
+    "AA2"=25,
+    "AA4"=40,
+    "AA7"=35,
+    "AA8"=55,
+    "C_PB1"=30,
+    "C_PB2"=20,
+    "C_PB3"=25
+)
